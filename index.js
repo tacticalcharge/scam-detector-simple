dbdbee9bb6d20d05c553ccf7670051e3278c57b4
tacticalcharge <143927612+tacticalcharge@users.noreply.github.com>
2026-08-02 13:30:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import "dotenv/config";
+const client = new Client({
+	  intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	  ]
+});
+
+client.on("clientReady", () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+	if (message.channel.id == process.env.CHANNEL_ID) {
+		message.author.ban({ reason: "Hacked account" });
+	}
+});
+client.login(process.env.TOKEN);
